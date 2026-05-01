d89853d30385a24944fd07a1d70690a6e5b038a0
artursbanzins <artursbanz@gmail.com>
2026-05-02 00:16:10 +0300

    1
    Sākotnējā augšupielāde uz GitHub, viss ir ļoti nepabeigts

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import sqlite3
+from pathlib import Path
+
+app = Flask(__name__)
+
+def get_db_connection():
+
+    db = Path(__file__).parent / "database.db"
+    conn = sqlite3.connect(db)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route("/")
+def sakums():
+    return render_template("home.html")
+
+@app.route("/sludinajumi")
+def sludinajumi():
+    conn = get_db_connection()
+    sludinajumi = conn.execute("SELECT * FROM sludinajumi").fetchall()
+    conn.close()
+    return render_template("sludinajumi.html", sludinajumi=sludinajumi)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
