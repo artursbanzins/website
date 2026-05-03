b04223e5f4f8a6c7398977b06dbe6f8d14823a85
artursbanzins <artursbanz@gmail.com>
2026-05-03 15:35:19 +0300

    1.4
    Pievienota individuālu sludinājumu lapa, jauna datubāzes tabula "engine"

--- app.py
+++ app.py
@@ -40,5 +40,24 @@ def sludinajumi(quality_name):
     conn.close()
     return render_template("category.html", sludinajumi=sludinajumi)
 
+@app.route("/<quality_name>/<int:sludinajums_id>")
+def sludinajums(quality_name, sludinajums_id):  # add sludinajums_id parameter
+    conn = get_db_connection()
+    sludinajums = conn.execute("""
+        SELECT sludinajumi.*, qualities.quality_name, manufacturers.name AS manufacturer_name,
+        
+        FROM sludinajumi
+        JOIN manufacturers
+        ON sludinajumi.manufacturer_id = manufacturers.id
+        JOIN qualities
+        ON sludinajumi.quality = qualities.id
+        WHERE qualities.quality_name = ?
+        AND sludinajumi.id = ?
+        """,
+        (quality_name, sludinajums_id),  # pass both variables
+        ).fetchone()  # fetchone since we want a single listing
+    conn.close()
+    return render_template("listing.html", sludinajums=sludinajums)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
